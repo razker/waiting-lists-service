a08fca079502f3adf55c133d7c286fba3c73847f
razker <razker@gmail.com>
2022-11-14 10:53:22 +0200

    fixed url

--- constants/frontend.ts
+++ constants/frontend.ts
@@ -1,8 +1,6 @@
 const FRONTEND_DEV_URLS = ["http://localhost:3000", "http://localhost:3001"];
 
-const FRONTEND_PROD_URLS = [
-  "https://waiting-lists-app-production.up.railway.app",
-];
+const FRONTEND_PROD_URLS = ["https://fdt-waiting-lists.up.railway.app"];
 
 export default process.env.NODE_ENV === "production"
   ? FRONTEND_PROD_URLS
