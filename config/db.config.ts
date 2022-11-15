081b615226bac2506cc0d624bbd9aba42799e7ce
razker <razker@gmail.com>
2022-11-15 18:26:58 +0200

    dotenv fix

--- config/db.config.ts
+++ config/db.config.ts
@@ -1,46 +1,45 @@
+require("dotenv").config();
 import * as Mongoose from "mongoose";
-require('dotenv').config()
 
 let database: Mongoose.Connection;
 
 export const connect = () => {
+  const url = process.env.MONGO_CONNECTION_STRING;
+  console.log(
+    "db.config | connect | process.env.MONGO_CONNECTION_STRING :::",
+    process.env.MONGO_CONNECTION_STRING
+  );
 
-    const url = process.env.MONGO_CONNECTION_STRING;
-    console.log("from connect: process.env.MONGO_CONNECTION_STRING :::",process.env.MONGO_CONNECTION_STRING)
+  if (database) {
+    return;
+  }
 
-    if (database) {
-        return;
-    }
-    
-    Mongoose.connect(url, {
-        useNewUrlParser: true,
-        useFindAndModify: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
-    });
-    
-    database = Mongoose.connection;
+  Mongoose.connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
 
-    database.once("open", async () => {
-        console.log("Connected to database");
-    });
-      
-    database.on("error", () => {
-        console.log("Error connecting to database");
-    });
+  database = Mongoose.connection;
 
+  database.once("open", async () => {
+    console.log("Connected to database");
+  });
+
+  database.on("error", () => {
+    console.log("Error connecting to database");
+  });
 };
 
 export const disconnect = () => {
-    
-    if (!database) {
-      return;
-    }
-    
-    Mongoose.disconnect();
+  if (!database) {
+    return;
+  }
 
-    database.once("close", async () => {
-        console.log("Diconnected  to database");
-    });
+  Mongoose.disconnect();
 
+  database.once("close", async () => {
+    console.log("Diconnected  to database");
+  });
 };
