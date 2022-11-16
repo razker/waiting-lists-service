1bd1270525612260612a2cb937d8461a9d7bead7
razker <razker@gmail.com>
2022-11-16 12:40:04 +0200

    CORS info

--- server.config.ts
+++ server.config.ts
@@ -1,8 +1,15 @@
 import CORS_WHITELIST from "./constants/frontend";
 
 export const corsOption = {
-  origin: (origin, callback) =>
-    CORS_WHITELIST.indexOf(origin) !== -1
+  origin: (origin, callback) => {
+    const originIsValid = CORS_WHITELIST.indexOf(origin) !== -1;
+
+    console.log(
+      `CORS from origin ${origin} is ${originIsValid ? "valid" : "invalid"}`
+    );
+
+    return originIsValid
       ? callback(null, true)
-      : callback(new Error("Not allowed by CORS")),
+      : callback(new Error("Not allowed by CORS"));
+  },
 };
