2bc1c7f659d1ab20b521fc7766ce8d72e469b265
razker <razker@gmail.com>
2022-11-16 09:44:08 +0200

    added new front url

--- constants/frontend.ts
+++ constants/frontend.ts
@@ -1,6 +1,9 @@
 const FRONTEND_DEV_URLS = ["http://localhost:3000", "http://localhost:3001"];
 
-const FRONTEND_PROD_URLS = ["https://fdt-waiting-lists.up.railway.app"];
+const FRONTEND_PROD_URLS = [
+  "https://fdt-waiting-lists.up.railway.app",
+  "https://waiting-lists-fdt.netlify.app",
+];
 
 export default process.env.NODE_ENV === "production"
   ? FRONTEND_PROD_URLS
